33a14a351fec81829bb0f21576b2966efc21c17c
Faris Widyantho <radenfaris@gmail.com>
2019-01-25 21:15:01 +0700

    detached

---
+++ android/app/src/main/java/com/fariswdev/old/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.fariswdev.old.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expc0eba5e006624b7190b20ae21a4ab5a6://192.168.1.126:19000";
+
+}
