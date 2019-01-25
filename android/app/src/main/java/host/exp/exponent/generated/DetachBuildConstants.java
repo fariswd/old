73759e2d7c4615e57de486a7cac70742f8c7ce43
Faris Widyantho <radenfaris@gmail.com>
2019-01-25 23:07:17 +0700

    detached

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp38293b581a194b098650be0edc41fe63://192.168.1.126:19000";
+
+}
