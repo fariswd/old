26e334438cd27e7f5e41962005ceeb79bd934683
Faris Widyantho <radenfaris@gmail.com>
2019-01-29 15:31:25 +0700

    onesignal success

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import OneSignal from 'react-native-onesignal';
+
 export default class App extends React.Component {
+  componentDidMount() {
+    console.log('-one', OneSignal)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.asdasjs to start working on your app!</Text>
       </View>
     );
   }
